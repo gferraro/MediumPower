c3cef2441fac56a13b2e8c23b153200bc68f0710
gferraro <g.ferraro22@gmail.com>
2026-05-18 15:14:29 +1200

    correct firmware

--- src/cptvwriter.py
+++ src/cptvwriter.py
@@ -47,7 +47,7 @@ def write_header(filename, headers,config,timestamp_micros, min_value = 0,max_va
         fw.string(ord(Field.MODEL), headers.model.encode())
         fw.string(ord(Field.BRAND),headers.brand.encode())
         fw.uint32(ord(Field.CAMERA_SERIAL),headers.serial)
-        fw.string(ord(Field.FIRMWARE),headers.brand.encode())
+        fw.string(ord(Field.FIRMWARE),headers.firmware.encode())
         if config.location.latitude != 0:
             fw.float32(ord(Field.LATITUDE), config.location.latitude)
         if config.location.longitude != 0:
